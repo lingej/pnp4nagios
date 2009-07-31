d6a5a4b98f236260306a8cf65fdca4efcfefa9be
Joerg Linge <pitchfork@pnp4nagios.org>
2009-07-31 14:02:11 +0200

    wrong graph source in views/popup.php

--- share/pnp/application/views/popup.php
+++ share/pnp/application/views/popup.php
@@ -1,7 +1,7 @@
 <table style="background-color:#FFF"><tr><td>
 <?php
-foreach ( $this->data->DS as $KEY=>$VAL){
-	$source = $VAL['DS'];
+foreach ( $this->data->STRUCT as $KEY=>$VAL){
+	$source = $VAL['SOURCE'];
 	echo "<tr><td>\n";
 	echo "<img width=\"".$imgwidth."\" src=\"/pnp4nagios/image?host=$host&srv=$srv&view=$view&source=$source\">\n";
 	echo "</td></tr>\n";
