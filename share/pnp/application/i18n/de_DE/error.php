ba35d14a67721bc01a258416509e63844eaccb18
Joerg Linge <pitchfork@pnp4nagios.org>
2009-08-13 19:26:11 +0200

    FAQ URL changed from pnp4nagios.org to docs.pnp4nagios.org

--- share/pnp/application/i18n/de_DE/error.php
+++ share/pnp/application/i18n/de_DE/error.php
@@ -1,18 +1,17 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 $lang = array
 (
-    'rrdtool-not-found' => 'RRDTool not found in %s. <a href="http://pnp4nagios.org/pnp/faq/1">Read FAQ online</a>',
-    'config-not-found' => 'Config File %s not found. <a href="http://pnp4nagios.org/pnp/faq/2">Read FAQ online</a>',
-    'perfdata-dir-empty' => 'Perfdata Directory "%s" is empty. Please check your Nagios Config. <a href="http://pnp4nagios.org/pnp/faq/3">Read FAQ online</a>',
-    'host-perfdata-dir-empty' => 'Perfdata Directory "%s" is empty. Please check your Nagios Config. <a href="http://pnp4nagios.org/pnp/faq/4">Read FAQ online</a>',
-    'perfdata-dir-for-host' => 'Perfdata Directory "%s" for Host "%s" does not exist. <a href="http://pnp4nagios.org/pnp/faq/5">Read FAQ online</a>',
-    'xml-not-found' => 'XML File "%s" not found. <a href="http://pnp4nagios.org/pnp/faq/6">Read FAQ online</a>',
-    'get-first-service' => 'Can´t find first service for host "%s". <a href="http://pnp4nagios.org/pnp/faq/7">Read FAQ online</a>',
-    'get-first-host' => 'Can´t find any Host. <a href="http://pnp4nagios.org/pnp/faq/8">Read FAQ online</a>',
-    'xml-structure-mismatch' => 'XML Structure mismatch. Found Version "%d" but should be "%d". <a href="http://pnp4nagios.org/pnp/faq/9">Read FAQ online</a>',
-    'save-rrd-image' => 'php fopen("%s") failed. <a href="http://pnp4nagios.org/pnp/faq/10">Read FAQ online</a>',
-	'xml-structure-without-version-tag' => 'XML Structure mismatch. Version Tag not found. <a href="http://pnp4nagios.org/pnp/faq/11">Read FAQ online</a>',
-    'template-without-opt' => 'Template %s does not provide array $opt[]. <a href="http://pnp4nagios.org/pnp/faq/12">Read FAQ online</a>',
-	'template-without-def' => 'Template %s does not provide array $def[]. <a href="http://pnp4nagios.org/pnp/faq/13">Read FAQ online</a>',
-
+    'rrdtool-not-found' => 'RRDTool not found in %s. <a href="http://docs.pnp4nagios.org/faq/1">Read FAQ online</a>',
+    'config-not-found' => 'Config File %s not found. <a href="http://docs.pnp4nagios.org/faq/2">Read FAQ online</a>',
+    'perfdata-dir-empty' => 'Perfdata Directory "%s" is empty. Please check your Nagios Config. <a href="http://docs.pnp4nagios.org/faq/3">Read FAQ online</a>',
+    'host-perfdata-dir-empty' => 'Perfdata Directory "%s" is empty. Please check your Nagios Config. <a href="http://docs.pnp4nagios.org/faq/4">Read FAQ online</a>',
+    'perfdata-dir-for-host' => 'Perfdata Directory "%s" for Host "%s" does not exist. <a href="http://docs.pnp4nagios.org/faq/5">Read FAQ online</a>',
+    'xml-not-found' => 'XML File "%s" not found. <a href="http://docs.pnp4nagios.org/faq/6">Read FAQ online</a>',
+    'get-first-service' => 'Can´t find first service for host "%s". <a href="http://docs.pnp4nagios.org/faq/7">Read FAQ online</a>',
+    'get-first-host' => 'Can´t find any Host. <a href="http://docs.pnp4nagios.org/faq/8">Read FAQ online</a>',
+    'xml-structure-mismatch' => 'XML Structure mismatch. Found Version "%d" but should be "%d". <a href="http://docs.pnp4nagios.org/faq/9">Read FAQ online</a>',
+    'save-rrd-image' => 'php fopen("%s") failed. <a href="http://docs.pnp4nagios.org/faq/10">Read FAQ online</a>',
+	'xml-structure-without-version-tag' => 'XML Structure mismatch. Version Tag not found. <a href="http://docs.pnp4nagios.org/faq/11">Read FAQ online</a>',
+    'template-without-opt' => 'Template %s does not provide array $opt[]. <a href="http://docs.pnp4nagios.org/faq/12">Read FAQ online</a>',
+	'template-without-def' => 'Template %s does not provide array $def[]. <a href="http://docs.pnp4nagios.org/faq/13">Read FAQ online</a>',
 );
