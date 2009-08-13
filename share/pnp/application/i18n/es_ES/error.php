55689bb53579c98dc4ec49e003302ddbe6fbde00
Carlos de Nova <cnova@p291.(none)>
2009-08-13 11:29:32 +0200

    Updated i18n spanish

--- share/pnp/application/i18n/es_ES/error.php
+++ share/pnp/application/i18n/es_ES/error.php
@@ -1,15 +1,15 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 $lang = array
 (
-    'rrdtool-not-found' => 'RRDTool no se encuentra en %s. <a href="http://pnp4nagios.org/pnp/faq/1">Read FAQ online</a>',
-    'config-not-found' => 'El fichero de configuración %s no se ha encontrado. <a href="http://pnp4nagios.org/pnp/faq/2">Read FAQ online</a>',
-	'perfdata-dir-empty' => 'El directorio de Perfdata "%s" está vacío. Compruebe la configuración de Nagios. <a href="http://pnp4nagios.org/pnp/faq/3">Read FAQ online</a>',
-    'host-perfdata-dir-empty' => 'El directorio de Perfdata "%s" está vacío. Compruebe la configuración de Nagios. <a href="http://pnp4nagios.org/pnp/faq/4">Read FAQ online</a>',
-    'perfdata-dir-for-host' => 'El directorio Perfdata "%s" para el Equipo "%s" no existe. <a href="http://pnp4nagios.org/pnp/faq/5">Read FAQ online</a>',
-    'xml-not-found' => 'Fichero XML "%s" no encontrado. <a href="http://pnp4nagios.org/pnp/faq/6">Read FAQ online</a>',
-    'get-first-service' => 'No puedo encontrar el primer servicio para el equipo "%s". <a href="http://pnp4nagios.org/pnp/faq/7">Read FAQ online</a>',
-    'get-first-host' => 'No puedo encuentrar ningún Equipo. <a href="http://pnp4nagios.org/pnp/faq/8">Read FAQ online</a>',
+    'rrdtool-not-found' => 'RRDTool no se encuentra en %s. <a href="http://pnp4nagios.org/pnp/faq/1">Leer FAQ en línea</a>',
+    'config-not-found' => 'El fichero de configuración %s no se ha encontrado. <a href="http://pnp4nagios.org/pnp/faq/2">Leer FAQ en línea</a>',
+	'perfdata-dir-empty' => 'El directorio de Perfdata "%s" está vacío. Compruebe la configuración de Nagios. <a href="http://pnp4nagios.org/pnp/faq/3">Leer FAQ en línea</a>',
+    'host-perfdata-dir-empty' => 'El directorio de Perfdata "%s" está vacío. Compruebe la configuración de Nagios. <a href="http://pnp4nagios.org/pnp/faq/4">Leer FAQ en línea</a>',
+    'perfdata-dir-for-host' => 'El directorio Perfdata "%s" para el Equipo "%s" no existe. <a href="http://pnp4nagios.org/pnp/faq/5">Leer FAQ en línea</a>',
+    'xml-not-found' => 'Fichero XML "%s" no encontrado. <a href="http://pnp4nagios.org/pnp/faq/6">Leer FAQ en línea</a>',
+    'get-first-service' => 'No puedo encontrar el primer servicio para el equipo "%s". <a href="http://pnp4nagios.org/pnp/faq/7">Leer FAQ en línea</a>',
+    'get-first-host' => 'No puedo encuentrar ningún Equipo. <a href="http://pnp4nagios.org/pnp/faq/8">Leer FAQ en línea</a>',
     'xml-structure-mismatch' => 'Error en la Estructura XML. Versión Encontrada "%d" pero debería ser "%d". <a href="http://pnp4nagios.org/pnp/faq/9">Read FAQ online</a>',
-    'save-rrd-image' => 'fallo en php fopen("%s"). <a href="http://pnp4nagios.org/pnp/es/faq/10">Read FAQ online</a>',
-	'xml-structure-without-version-tag' => 'XML Structure mismatch. Version Tag not found. <a href="http://pnp4nagios.org/pnp/faq/11">Read FAQ online</a>',
+    'save-rrd-image' => 'fallo en php fopen("%s"). <a href="http://pnp4nagios.org/pnp/es/faq/10">Leer FAQ en línea</a>',
+	'xml-structure-without-version-tag' => 'Error en la estructura XML. Etiqueta de Versión no encontrada. <a href="http://pnp4nagios.org/pnp/faq/11">Leer FAQ en línea</a>',
 );
