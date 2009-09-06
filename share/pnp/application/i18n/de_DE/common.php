21f1c53efd7358c72a2cddd4f2886facc4319cd4
Wolfgang <wolfgang@debian5.(none)>
2009-09-06 17:06:53 +0200

    Typos / verify_pnp_config.in: PNP-dir option

--- share/pnp/application/i18n/de_DE/common.php
+++ share/pnp/application/i18n/de_DE/common.php
@@ -1,17 +1,17 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php defined('SYSPATH') OR die('kein direkter Zugang erlaubt.');
 $lang = array
 (
-    'host' => 'Host: %s',
+	'host' => 'Host: %s',
 	'service' => 'Service: %s',
 	'datasource' => 'Datasource: %s',
 	'zoom-header' => 'Zoom',
 	'status-box-header' => 'Status',
-    'search-box-header' => 'Search',
-    'icon-box-header' => 'Actions',
-	'basket-box-header' => 'My Basket',
-    'timerange-box-header' => 'Timeranges',
-    'service-box-header' => 'Services',
+	'search-box-header' => 'Suche',
+	'icon-box-header' => 'Aktionen',
+	'basket-box-header' => 'Mein Korb',
+	'timerange-box-header' => 'Zeitbereiche',
+	'service-box-header' => 'Services',
 	'pages-box-header' => 'Pages',
-    'nagios-summary-link-title' => 'Most Recent Alerts for this Timerange',
-    'nagios-avail-link-title'   => 'Nagios Availability Report for this Timerange',
+	'nagios-summary-link-title' => 'aktuellste Alarme für diesen Zeitbereich',
+	'nagios-avail-link-title'   => 'Nagios-Verfügbarkeitsbericht für diesen Zeitbereich',
 );
