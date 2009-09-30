21a3abc255c3bb9e9e8fda19ae256b0972dad7e6
Joerg Linge <pitchfork@pnp4nagios.org>
2009-09-30 10:51:18 +0200

    Catch exception on empty basket sessions

--- share/pnp/application/controllers/page.php
+++ share/pnp/application/controllers/page.php
@@ -77,6 +77,9 @@ class Page_Controller extends System_Controller  {
 			// Icon Box	
 			$this->template->page->icon_box      = $this->add_view('icon_box');
 			$this->template->page->icon_box->position = "basket";
-        }
+        }else{
+			// FIXME i18n 
+			throw new Kohana_User_Exception('Basket Session', "Session is empty");
+		}
 	}
 }
