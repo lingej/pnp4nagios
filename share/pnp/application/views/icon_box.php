c01790d3ca22848e5b6e65164877344a94f36e20
Joerg Linge <pitchfork@pnp4nagios.org>
2009-10-03 18:06:55 +0200

    Timerange selector

--- share/pnp/application/views/icon_box.php
+++ share/pnp/application/views/icon_box.php
@@ -6,16 +6,7 @@
 <div class="p4 ui-widget-content ui-corner-bottom" >
 <?php
 if($position == "graph"){
-echo "<script type=\"text/javascript\">
-	$(function() {
-		var currentTime = new Date()
-		var hours = (currentTime.getHours() * 60 * 60)
-		var minutes = (currentTime.getMinutes() * 60)
-		var sec = (hours + minutes)
-		$(\"#datepicker\").datepicker({showOn: 'button', buttonImage: '".url::base()."media/images/calendar.png', dateFormat: '@', buttonImageOnly: true, onSelect: function(dateText, inst) { window.location.href = '".url::base()."graph".$this->url."&end=' + (dateText / 1000 + sec) }});
-	});
-	</script>
-<input type=\"hidden\" id=\"datepicker\">\n";
+	echo "<a href=\"#\" id=\"button\"><img src=\"".url::base()."media/images/calendar.png\"></a>"; 
 }
 if($this->config->conf['use_fpdf'] == 1 && $position == "graph"){
 	echo "<a title=\"PDF View\" href=\"".url::base()."pdf".$this->url."&view=".$this->view."\"><img src=\"".url::base()."media/images/PDF_32.png\"></a>\n";
