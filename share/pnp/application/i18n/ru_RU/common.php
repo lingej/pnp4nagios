d878a99703449328699fb17e29a1ef3072125b5d
Joerg Linge <pitchfork@pnp4nagios.org>
2009-10-11 20:32:19 +0200

    Russian language support contributed by CoolCold

---
+++ share/pnp/application/i18n/ru_RU/common.php
@@ -0,0 +1,24 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+$lang = array
+(
+    'datasource' => 'Источник данных: %s',
+    'host' => 'Хост: %s',
+    'service' => 'Служба: %s',
+    'zoom-header' => 'Детализация',
+    'status-box-header' => 'Статус',
+    'search-box-header' => 'Поиск',
+    'icon-box-header' => 'Действия',
+    'basket-box-header' => 'Мой набор',
+    'timerange-box-header' => 'Интервалы времени',
+    'service-box-header' => 'Службы',
+    'pages-box-header' => 'Страницы',
+    'nagios-summary-link-title' => 'Последние алерты за данный промежуток',
+    'nagios-avail-link-title'   => 'Сводка доступности за данный промежуток',
+    'timerange-selector-legend' => 'Выберите промежуток',
+    'timerange-selector-title'  => 'Установка собственного интервала времени',
+    'timerange-selector-submit-button' => 'ввести',
+    'timerange-selector-clear-button'  => 'очистить',
+    'timerange-selector-link'          => 'Собственный интервал',
+	'start' => 'Начало',
+	'end' => 'Окончание',
+);
