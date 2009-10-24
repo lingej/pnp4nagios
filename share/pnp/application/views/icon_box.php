0923b587430c131046b126558cfa601e380001a7
Joerg Linge <pitchfork@pnp4nagios.org>
2009-10-24 19:24:56 +0200

    store pnp internal statistics

--- share/pnp/application/views/icon_box.php
+++ share/pnp/application/views/icon_box.php
@@ -6,23 +6,24 @@
 <div class="p4 ui-widget-content ui-corner-bottom" >
 <?php
 if($this->config->conf['use_calendar']){
-	echo "<a href=\"#\" id=\"button\"><img src=\"".url::base()."media/images/calendar.png\"></a>"; 
+	echo "<a title=\"".Kohana::lang('common.title-calendar-link')."\" href=\"#\" id=\"button\"><img src=\"".url::base()."media/images/calendar.png\"></a>"; 
 }
 if($this->config->conf['use_fpdf'] == 1 && $position == "graph"){
-	echo "<a title=\"PDF View\" href=\"".url::base()."pdf".$this->url."&view=".$this->view."\"><img src=\"".url::base()."media/images/PDF_32.png\"></a>\n";
+	echo "<a title=\"".Kohana::lang('common.title-pdf-link')."\" href=\"".url::base()."pdf".$this->url."&view=".$this->view."\"><img src=\"".url::base()."media/images/PDF_32.png\"></a>\n";
 }
 if($this->config->conf['use_fpdf'] == 1 && $position == "basket"){
-	echo "<a title=\"PDF View\" href=\"".url::base()."pdf/basket?view=".$this->view."\"><img src=\"".url::base()."media/images/PDF_32.png\"></a>\n";
+	echo "<a title=\"".Kohana::lang('common.title-pdf-link')."\" href=\"".url::base()."pdf/basket?view=".$this->view."\"><img src=\"".url::base()."media/images/PDF_32.png\"></a>\n";
 }
 if($this->config->conf['use_fpdf'] == 1 && $position == "page"){
-	echo "<a title=\"PDF View\" href=\"".url::base()."pdf/page/".$this->page."?view=".$this->view."\"><img src=\"".url::base()."media/images/PDF_32.png\"></a>\n";
+	echo "<a title=\"".Kohana::lang('common.title-pdf-link')."\" href=\"".url::base()."pdf/page/".$this->page."?view=".$this->view."\"><img src=\"".url::base()."media/images/PDF_32.png\"></a>\n";
 }
 if($this->config->conf['show_xml_icon'] == 1 && $position == "graph"){
-	echo "<a title=\"XML View\" href=\"".url::base()."xml".$this->url."\"><img src=\"".url::base()."media/images/XML_32.png\"></a>\n";
+	echo "<a title=\"".Kohana::lang('common.title-xml-link')."\" href=\"".url::base()."xml".$this->url."\"><img src=\"".url::base()."media/images/XML_32.png\"></a>\n";
 }
 if($this->data->getFirstPage() && $this->isAuthorizedFor('pages') ){
-	echo "<a title=\"Goto Pages\" href=\"".url::base()."page\"><img src=\"".url::base()."media/images/pages.png\"></a>\n";
+	echo "<a title=\"".Kohana::lang('common.title-pages-link')."\" href=\"".url::base()."page\"><img src=\"".url::base()."media/images/pages.png\"></a>\n";
 }
+echo "<a title=\"".Kohana::lang('common.title-statistics-link')."\" href=\"".url::base()."graph?host=.pnp-internal&srv=runtime\"><img src=\"".url::base()."media/images/stats.png\"></a>\n";
 ?>
 </div>
 </div><p>
