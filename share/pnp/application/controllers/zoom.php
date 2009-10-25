09286f152315b3b918609e67c7791c87de423d8a
Joerg Linge <pitchfork@pnp4nagios.org>
2009-10-25 20:06:32 +0100

    store zoomed range as custom time range for later usage

--- share/pnp/application/controllers/zoom.php
+++ share/pnp/application/controllers/zoom.php
@@ -20,26 +20,30 @@ class Zoom_Controller extends System_Controller  {
 	public function index()
 	{
 
-		$start   = $this->input->get('start');
-		$end     = $this->input->get('end');
-		$source  = $this->input->get('source');
-		$view    = "";
+		$this->start   = $this->input->get('start');
+		$this->end     = $this->input->get('end');
+		$this->source  = $this->input->get('source');
+		$this->view    = "";
 
         if(isset($_GET['view']) && $_GET['view'] != "" ){
-			$view = pnp::clean($_GET['view']);
+			$this->view = pnp::clean($_GET['view']);
 		}else{
-			$view = $this->config->conf['overview-range'];
+			$this->view = $this->config->conf['overview-range'];
 		}
 
-		$this->data->getTimeRange($start,$end,$view);
+		$this->data->getTimeRange($this->start,$this->end,$this->view);
 
 		if(isset($this->host) && isset($this->service)){
+            if($this->start && $this->end ){
+                    $this->session->set("start", $this->start);
+                    $this->session->set("end", $this->end);
+            }
 			$this->template->host    = $this->host;
 			$this->template->srv     = $this->service;
-			$this->template->view    = $view;
-			$this->template->source  = $source;
-			$this->template->end     = $end;
-			$this->template->start   = $start;
+			$this->template->view    = $this->view;
+			$this->template->source  = $this->source;
+			$this->template->end     = $this->end;
+			$this->template->start   = $this->start;
 			$this->template->graph_height = $this->config->conf['graph_height'];
 			$this->template->graph_width  = $this->config->conf['graph_width'];
 		}else{
