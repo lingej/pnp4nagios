77f68e623a255b719bc9e5568051925aaf45bfbf
Joerg Linge <pitchfork@pnp4nagios.org>
2009-10-31 11:06:00 +0100

    redesign of pnp internal statistics

--- share/pnp/templates.dist/pnp-runtime.php
+++ share/pnp/templates.dist/pnp-runtime.php
@@ -33,32 +33,46 @@ $def[1] .= "GPRINT:t_var1:MAX:\"%6.2lf$UNIT[1] max\\n\" ";
 #
 # Lines processed and RRD errors 
 #
-$opt[2] = "--vertical-label \"Counter\" --title \"Number of updates per run\" ";
+$opt[2] = "--vertical-label \"Counter\" --title \"Number of updates\" ";
 #
 #
 #
-$ds_name[2] = "Errors and updates per run";
+$ds_name[2] = "Errors and updates";
 $def[2] =  "DEF:var1=$RRDFILE[2]:$DS[2]:AVERAGE " ;
-$def[2] .= "DEF:var2=$RRDFILE[3]:$DS[3]:AVERAGE " ;
-$def[2] .= "DEF:var3=$RRDFILE[4]:$DS[4]:AVERAGE " ;
-$def[2] .= "DEF:var4=$RRDFILE[5]:$DS[5]:AVERAGE " ;
-$def[2] .= "AREA:var1#6666ff:\"Updates\\t\" " ;
-$def[2] .= "LINE1:var1#0000CC " ;
+$def[2] .= "LINE1:var1#6666ff:\"".ucfirst($LABEL[2])."\\t\" " ;
 $def[2] .= "GPRINT:var1:LAST:\"%.0lf$UNIT[2] last\\t\" " ;
 $def[2] .= "GPRINT:var1:AVERAGE:\"%.0lf$UNIT[2] avg\\t\" " ;
 $def[2] .= "GPRINT:var1:MAX:\"%.0lf$UNIT[2] max\\n\" ";
-$def[2] .= "LINE1:var2#ff3333:\"Errors\\t\" " ;
+
+$def[2] .= "DEF:var2=$RRDFILE[3]:$DS[3]:AVERAGE " ;
+$def[2] .= "LINE1:var2#ff3333:\"".ucfirst($LABEL[3])."\\t\" " ;
 $def[2] .= "GPRINT:var2:LAST:\"%.0lf$UNIT[3] last\\t\" " ;
 $def[2] .= "GPRINT:var2:AVERAGE:\"%.0lf$UNIT[3] avg\\t\" " ;
 $def[2] .= "GPRINT:var2:MAX:\"%.0lf$UNIT[3] max\\n\" ";
-$def[2] .= "LINE1:var3#ffff33:\"Invalid\\t\" " ;
+
+$def[2] .= "DEF:var3=$RRDFILE[4]:$DS[4]:AVERAGE " ;
+$def[2] .= "LINE1:var3#ffff33:\"".ucfirst($LABEL[4])."\\t\" " ;
 $def[2] .= "GPRINT:var3:LAST:\"%.0lf$UNIT[4] last\\t\" " ;
 $def[2] .= "GPRINT:var3:AVERAGE:\"%.0lf$UNIT[4] avg\\t\" " ;
 $def[2] .= "GPRINT:var3:MAX:\"%.0lf$UNIT[4] max\\n\" ";
-$def[2] .= "LINE1:var4#66ff33:\"Skipped\\t\" " ;
+
+$def[2] .= "DEF:var4=$RRDFILE[5]:$DS[5]:AVERAGE " ;
+$def[2] .= "LINE1:var4#66ff33:\"".ucfirst($LABEL[5])."\\t\" " ;
 $def[2] .= "GPRINT:var4:LAST:\"%.0lf$UNIT[5] last\\t\" " ;
 $def[2] .= "GPRINT:var4:AVERAGE:\"%.0lf$UNIT[5] avg\\t\" " ;
 $def[2] .= "GPRINT:var4:MAX:\"%.0lf$UNIT[5] max\\n\" ";
+
+$def[2] .= "DEF:var5=$RRDFILE[6]:$DS[6]:AVERAGE " ;
+$def[2] .= "LINE1:var5#66ff33:\"".ucfirst($LABEL[6])."\\t\" " ;
+$def[2] .= "GPRINT:var5:LAST:\"%.0lf$UNIT[6] last\\t\" " ;
+$def[2] .= "GPRINT:var5:AVERAGE:\"%.0lf$UNIT[6] avg\\t\" " ;
+$def[2] .= "GPRINT:var5:MAX:\"%.0lf$UNIT[6] max\\n\" ";
+
+$def[2] .= "DEF:var6=$RRDFILE[7]:$DS[7]:AVERAGE " ;
+$def[2] .= "LINE1:var6#66ff33:\"".ucfirst($LABEL[7])."\\t\" " ;
+$def[2] .= "GPRINT:var6:LAST:\"%.0lf$UNIT[7] last\\t\" " ;
+$def[2] .= "GPRINT:var6:AVERAGE:\"%.0lf$UNIT[7] avg\\t\" " ;
+$def[2] .= "GPRINT:var6:MAX:\"%.0lf$UNIT[7] max\\n\" ";
 
 
 ?>
