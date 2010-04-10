3650bcbf5c2e8513caf3fbfbc612f6df957b47b2
Joerg Linge <pitchfork@pnp4nagios.org>
2010-04-10 17:06:42 +0200

    New json controller used to gather informations about a given host/service

---
+++ share/pnp/application/controllers/json.php
@@ -0,0 +1,75 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * Json controller.
+ *
+ * @package    PNP4Nagios
+ * @author     Joerg Linge
+ * @license    GPL
+ */
+class Json_Controller extends System_Controller  {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->host      = $this->input->get('host');
+        $this->service   = $this->input->get('srv');
+    }
+
+    public function index(){
+        // Disable auto-rendering
+        $this->auto_render = FALSE;
+        if($this->view  == ""){
+            $this->view = $this->config->conf['overview-range'];
+        }
+        // Service Details
+        if($this->host != "" && $this->service != ""){
+            $this->service = pnp::clean($this->service);
+            $this->host    = pnp::clean($this->host);
+            $services      = $this->data->getServices($this->host);
+            $this->data->buildDataStruct($this->host,$this->service,$this->view);
+            $i = 0;
+            $json = array();
+            foreach($this->data->STRUCT as $struct){
+                $json[$i]['image_url']   = "host=".$struct['MACRO']['HOSTNAME']."&srv=".$struct['MACRO']['SERVICEDESC']."&source=".$struct['SOURCE']."&view=".$struct['VIEW'];
+                $json[$i]['ds_name']     = $struct['ds_name'];
+                $json[$i]['start']       = $struct['TIMERANGE']['start'];
+                $json[$i]['end']         = $struct['TIMERANGE']['end'];
+                $json[$i]['title']       = $struct['TIMERANGE']['title'];
+                $i++;
+            }
+            print json_encode($json);
+        // Host Overview
+        }elseif($this->host != ""){
+            $this->host     = pnp::clean($this->host);
+            $services = $this->data->getServices($this->host);
+            foreach($services as $service){
+                if($service['state'] == 'active'){
+                    $this->data->buildDataStruct($this->host,$service['name'],$this->view);
+                }    
+            }
+            $i = 0;
+            $json = array();
+            foreach($this->data->STRUCT as $struct){
+                $json[$i]['image_url']   = "host=".$struct['MACRO']['HOSTNAME']."&srv=".$struct['MACRO']['SERVICEDESC']."&source=".$struct['SOURCE']."&view=".$struct['VIEW'];
+                $json[$i]['servicedesc'] = $struct['MACRO']['SERVICEDESC'];
+                $json[$i]['ds_name']     = $struct['ds_name'];
+                $json[$i]['start']       = $struct['TIMERANGE']['start'];
+                $json[$i]['end']         = $struct['TIMERANGE']['end'];
+                $json[$i]['title']       = $struct['TIMERANGE']['title'];
+                $i++;
+            }
+            print json_encode($json);
+        }else{
+            $this->hosts = $this->data->getHosts();
+            $i = 0;
+            $json = array();
+            foreach($this->hosts as $host){
+                if($host['state'] == "active"){
+                    $json[$i]['hostname'] = $host['name'];
+                    $i++;
+                }
+            }
+            print json_encode($json);
+        }
+    }
+}
