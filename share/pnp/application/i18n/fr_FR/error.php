142050fefecd78b320afd436da69ecac777b834b
Joerg Linge <pitchfork@pnp4nagios.org>
2010-05-07 11:02:43 +0200

    New i18n files for fr_FR (Yannig Parre)

---
+++ share/pnp/application/i18n/fr_FR/error.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+$lang = array
+(
+    'rrdtool-not-found' => 'RRDTool non trouvé dans %s. <a href="http://docs.pnp4nagios.org/faq/1">Lire la FAQ</a>',
+    'config-not-found' => 'Fichier de config %s non trouvé. <a href="http://docs.pnp4nagios.org/faq/2">Lire la FAQ</a>',
+    'perfdata-dir-empty' => 'Répertoire perfdata "%s" vide. Merci de vérifier la configuration de Nagios. <a href="http://docs.pnp4nagios.org/faq/3">Lire la FAQ</a>',
+    'host-perfdata-dir-empty' => 'Répertoire perfdata "%s" vide. Merci de vérifier la configuration de Nagios. <a href="http://docs.pnp4nagios.org/faq/4">Lire la FAQ</a>',
+    'perfdata-dir-for-host' => 'Répertoire perfdata "%s" du serveur "%s" n\'existe pas. <a href="http://docs.pnp4nagios.org/faq/5">Lire la FAQ</a>',
+    'xml-not-found' => 'Fichier XML "%s" non trouvé. <a href="http://docs.pnp4nagios.org/faq/6">Lire la FAQ</a>',
+    'get-first-service' => 'Impossible de trouver le premier service du serveur "%s". <a href="http://docs.pnp4nagios.org/faq/7">Lire la FAQ</a>',
+    'get-first-host' => 'Impossible de trouver un serveur. <a href="http://docs.pnp4nagios.org/faq/8">Lire la FAQ</a>',
+    'xml-structure-mismatch' => 'Structure XML incorrecte. Version trouvé "%d" mais version attendu "%d". <a href="http://docs.pnp4nagios.org/faq/9">Lire la FAQ</a>',
+    'save-rrd-image' => 'Échec de la fonction php fopen("%s"). <a href="http://docs.pnp4nagios.org/faq/10">Lire la FAQ</a>',
+    'xml-structure-without-version-tag' => 'Structure XML incorrect. Balise de version introuvable dans "%s". <a href="http://docs.pnp4nagios.org/faq/11">Lire la FAQ</a>',
+    'template-without-opt' => 'Le template %s ne renvoie pas de tableau $opt[]. <a href="http://docs.pnp4nagios.org/faq/12">Lire la FAQ</a>',
+    'template-without-def' => 'Le template %s ne renvoie pas de tableau $def[]. <a href="http://docs.pnp4nagios.org/faq/13">Lire la FAQ</a>',
+    'no-data-for-page' => 'Désolé, mais impossible de trouver des données en utilisant le fichier de configuration "%s", <a href="http://docs.pnp4nagios.org/faq/14">Lire la FAQ</a>',
+    'page-not-readable' => 'Impossible de lire le fichier de configuration "%s" ou fichier introuvable. <a href="http://docs.pnp4nagios.org/faq/15">Lire la FAQ</a>',
+    'auth-pages' => 'Vous n\'êtes pas autorisé de voir "pages" <a href="http://docs.pnp4nagios.org/faq/16">Lire la FAQ</a>',
+    'page-config-dir' => 'Pas de fichier de configuration pour page dans "%s" <a href="http://docs.pnp4nagios.org/faq/17">Lire la FAQ</a>',
+    'xport-host-service' => 'Le contrôleur Xport nécessite les paramètres "host" et "srv" en paramètres d\'URL. <a href="http://docs.pnp4nagios.org/faq/18">Lire la FAQ</a>',
+	'mod-rewrite' => 'Module Apache Rewrite désactivé. <a href="http://docs.pnp4nagios.org/faq/19">Lire la FAQ</a>',
+);
