eb1bb9a4bb265ea0ba7194fc436ff01e4067523e
Joerg Linge <pitchfork@pnp4nagios.org>
2010-05-15 19:39:00 +0200

    check_disk.php fixed

--- share/pnp/templates.dist/check_disk.php
+++ share/pnp/templates.dist/check_disk.php
@@ -5,24 +5,22 @@
 #
 # RRDtool Options
 
-foreach ($DS as $key=>$val) {
-	$ds_name[$key] = str_replace("_","/",$NAME[$key]);
-	$opt[$key] = "--vertical-label MB -l 0 -u $MAX[$key] --title \"Filesystem $ds_name[$key]\" ";
-	#
-	#
+foreach ($this->DS as $KEY=>$VAL) {
+	$ds_name[$KEY] = str_replace("_","/",$VAL['NAME']);
+	$opt[$KEY] = "--vertical-label MB -l 0 -u " . $VAL['MAX'] . " --title \"Filesystem " . $ds_name[$KEY] . "\" ";
 	# Graph Definitions
-	$def[$key]  = "DEF:var1=$RRDFILE[$key]:$DS[$key]:AVERAGE "; 
-	$def[$key] .= "AREA:var1#c6c6c6:\"$servicedesc\\n\" "; 
-	$def[$key] .= "LINE1:var1#003300: "; 
-	$def[$key] .= "HRULE:$MAX[$key]#003300:\"Size $MAX[$key] MB \" ";
-	if ($WARN[$key] != "") {  
-		$def[$key] .= "HRULE:$WARN[$key]#ffff00:\"Warning on $WARN[$key] MB \" ";
+	$def[$KEY]  = "DEF:var1=" . $VAL['RRDFILE'] . ":" . $VAL['DS'] .":AVERAGE "; 
+	$def[$KEY] .= "AREA:var1#c6c6c6:\"" . $ds_name[$KEY] . "\\l\" "; 
+	$def[$KEY] .= "LINE1:var1#003300: "; 
+	$def[$KEY] .= "HRULE:" . $VAL['MAX'] . "#003300:\"Size " . $VAL['MAX'] . "  MB \" ";
+	if ($VAL['WARN'] != "") {  
+		$def[$KEY] .= "HRULE:" . $VAL['WARN'] . "#ffff00:\"Warning on " . $VAL['WARN'] . " MB \" ";
 	}
-	if ($CRIT[$key] != "") {  
-		$def[$key] .= "HRULE:$CRIT[$key]#ff0000:\"Critical on $CRIT[$key] MB \\n\" ";       
+	if ($VAL['CRIT'] != "") {  
+		$def[$KEY] .=  "HRULE:" . $VAL['CRIT'] . "#ff0000:\"Critical on " . $VAL['CRIT'] . " MB \\n\" ";       
 	}
-	$def[$key] .= "GPRINT:var1:LAST:\"%6.2lf MB of $MAX[$key] MB used \\n\" ";
-	$def[$key] .= "GPRINT:var1:MAX:\"%6.2lf MB max used \\n\" ";
-	$def[$key] .= "GPRINT:var1:AVERAGE:\"%6.2lf MB avg used\" ";
+	$def[$KEY] .= "GPRINT:var1:LAST:\"%6.2lf MB of " . $VAL['MAX'] . " MB used \\n\" ";
+	$def[$KEY] .= "GPRINT:var1:MAX:\"%6.2lf MB max used \\n\" ";
+    $def[$KEY] .= "GPRINT:var1:AVERAGE:\"%6.2lf MB avg used\" ";
 }
 ?>
