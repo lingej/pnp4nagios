a75d940f4495bdd5fa105a7f7e1f0d917d901f94
Joerg Linge <pitchfork@pnp4nagios.org>
2010-06-03 19:52:09 +0200

    check_fping fix ( by CoolCold )

--- share/pnp/templates.dist/check_fping.php
+++ share/pnp/templates.dist/check_fping.php
@@ -32,32 +32,34 @@ if($CRIT[2] != ""){
 }
 #
 # Packets Lost
-$ds_name[2] = "Packets Lost";
-$opt[2] = "--vertical-label \"Packets lost\" -l0 -u105 --title \"Packets lost\" ";
+if (array_key_exists(2,$DS)) {
+    
+    $ds_name[2] = "Packets Lost";
+    $opt[2] = "--vertical-label \"Packets lost\" -l0 -u105 --title \"Packets lost\" ";
 
-$def[2]  =  "DEF:var1=$RRDFILE[1]:$DS[1]:AVERAGE " ;
-$def[2] .=  "CDEF:sp1=var1,100,/,12,* " ;
-$def[2] .=  "CDEF:sp2=var1,100,/,30,* " ;
-$def[2] .=  "CDEF:sp3=var1,100,/,50,* " ;
-$def[2] .=  "CDEF:sp4=var1,100,/,70,* " ;
+    $def[2]  =  "DEF:var1=$RRDFILE[1]:$DS[1]:AVERAGE " ;
+    $def[2] .=  "CDEF:sp1=var1,100,/,12,* " ;
+    $def[2] .=  "CDEF:sp2=var1,100,/,30,* " ;
+    $def[2] .=  "CDEF:sp3=var1,100,/,50,* " ;
+    $def[2] .=  "CDEF:sp4=var1,100,/,70,* " ;
 
-$def[2] .= "AREA:var1#FF5C00:\"Packets lost \" " ;
-$def[2] .= "AREA:sp4#FF7C00: " ;
-$def[2] .= "AREA:sp3#FF9C00: " ;
-$def[2] .= "AREA:sp2#FFBC00: " ;
-$def[2] .= "AREA:sp1#FFDC00: " ;
+    $def[2] .= "AREA:var1#FF5C00:\"Packets lost \" " ;
+    $def[2] .= "AREA:sp4#FF7C00: " ;
+    $def[2] .= "AREA:sp3#FF9C00: " ;
+    $def[2] .= "AREA:sp2#FFBC00: " ;
+    $def[2] .= "AREA:sp1#FFDC00: " ;
 
-$def[2] .= "GPRINT:var1:LAST:\"%6.2lg $UNIT[1] last \" " ;
-$def[2] .= "GPRINT:var1:MAX:\"%6.2lg $UNIT[1] max \" " ;
-$def[2] .= "GPRINT:var1:AVERAGE:\"%6.2lg $UNIT[1] avg \\n\" " ;
-$def[2] .= "LINE1:var1#000000: " ;
-$def[2] .= "HRULE:100#000000:\"\" " ;
-if($WARN[1] != ""){
-  $def[2] .= "HRULE:".$WARN[1]."#FFFF00:\"Warning ".$WARN[1].$UNIT[1]." \" " ;
+    $def[2] .= "GPRINT:var1:LAST:\"%6.2lg $UNIT[1] last \" " ;
+    $def[2] .= "GPRINT:var1:MAX:\"%6.2lg $UNIT[1] max \" " ;
+    $def[2] .= "GPRINT:var1:AVERAGE:\"%6.2lg $UNIT[1] avg \\n\" " ;
+    $def[2] .= "LINE1:var1#000000: " ;
+    $def[2] .= "HRULE:100#000000:\"\" " ;
+    if($WARN[1] != ""){
+        $def[2] .= "HRULE:".$WARN[1]."#FFFF00:\"Warning ".$WARN[1].$UNIT[1]." \" " ;
+    }
+    if($CRIT[1] != ""){
+        $def[2] .= "HRULE:".$CRIT[1]."#FF0000:\"Critical ".$CRIT[1].$UNIT[1]." \" " ;
+    }
 }
-if($CRIT[1] != ""){
-  $def[2] .= "HRULE:".$CRIT[1]."#FF0000:\"Critical ".$CRIT[1].$UNIT[1]." \" " ;
-}
-
 ?>
 
