c8f6f32a7a3b4fac96a89e3b87bf56d34f3dbcc4
Joerg Linge <pitchfork@pnp4nagios.org>
2010-06-24 18:55:07 +0200

    language icons

--- share/pnp/application/views/docs_box.php
+++ share/pnp/application/views/docs_box.php
@@ -1,16 +1,17 @@
 <!-- Docs Menu Start -->
 <div class="ui-widget">
- <div class="p2 ui-widget-header ui-corner-top">
- <?php echo Kohana::lang('common.icon-box-header') ?>
- </div>
- <div class="p4 ui-widget-content ui-corner-bottom" >
-<?php echo "<a title=\"".Kohana::lang('common.title-home-link')."\" href=\"".url::base(TRUE)."graph\"><img class=\"icon\" src=\"".url::base()."media/images/home.png\"></a>\n";?>
-<?php echo "<a title=\"".Kohana::lang('common.title-docs-link')."\" href=\"".url::base(TRUE)."docs\"><img class=\"icon\" src=\"".url::base()."media/images/docs.png\"></a>\n";?>
-<?php
+<div class="p2 ui-widget-header ui-corner-top">
+<?php echo Kohana::lang('common.icon-box-header') ?>
+</div>
+<div class="p4 ui-widget-content ui-corner-bottom" >
+<?php 
+echo "<a title=\"".Kohana::lang('common.title-home-link')."\" href=\"".url::base(TRUE)."graph\"><img class=\"icon\" src=\"".url::base()."media/images/home.png\"></a>\n";
+echo "<a title=\"".Kohana::lang('common.title-docs-link')."\" href=\"".url::base(TRUE)."docs\"><img class=\"icon\" src=\"".url::base()."media/images/docs.png\"></a>\n";
 foreach ( $this->doc_language as $lang ){
-    echo "<a href=\"".url::base(TRUE)."docs/view/".$lang."/start\">".$lang."</a> \n";
-}?>
- </div>
+    echo "<a href=\"".url::base(TRUE)."docs/view/".$lang."/start\"><img class=\"icon\" src=\"".url::base(TRUE)."media/images/".$lang.".png\"></a> \n";
+}
+?>
+</div>
 </div>
 <p>
 <div class="ui-widget">
