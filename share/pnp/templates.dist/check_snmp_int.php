25ccc66aadf192a029d687e791c4d276dbbbf912
Joerg Linge <pitchfork@pnp4nagios.org>
2011-07-25 11:15:20 +0200

    fixed check_snmp_int.php UOM

--- share/pnp/templates.dist/check_snmp_int.php
+++ share/pnp/templates.dist/check_snmp_int.php
@@ -3,17 +3,17 @@
 # Copyright (c) 2006-2010 Joerg Linge (http://www.pnp4nagios.org)
 # Plugin: check_snmp_int.pl (COUNTER)
 #
-$opt[1] = " --vertical-label \"Traffic\" -b 1000 --title \"Interface Traffic for $hostname / $servicedesc\" ";
+$opt[1] = " --vertical-label \"Bytes\" -b 1000 --title \"Interface Traffic for $hostname / $servicedesc\" ";
 $def[1] = "DEF:var1=$RRDFILE[1]:$DS[1]:AVERAGE " ;
 $def[1] .= "DEF:var2=$RRDFILE[2]:$DS[2]:AVERAGE " ;
 $def[1] .= "LINE1:var1#003300:\"in  \" " ;
-$def[1] .= "GPRINT:var1:LAST:\"%7.2lf %Sb/s last\" " ;
-$def[1] .= "GPRINT:var1:AVERAGE:\"%7.2lf %Sb/s avg\" " ;
-$def[1] .= "GPRINT:var1:MAX:\"%7.2lf %Sb/s max\\n\" " ;
+$def[1] .= "GPRINT:var1:LAST:\"%7.2lf %SB/s last\" " ;
+$def[1] .= "GPRINT:var1:AVERAGE:\"%7.2lf %SB/s avg\" " ;
+$def[1] .= "GPRINT:var1:MAX:\"%7.2lf %SB/s max\\n\" " ;
 $def[1] .= "LINE1:var2#00ff00:\"out \" " ;
-$def[1] .= "GPRINT:var2:LAST:\"%7.2lf %Sb/s last\" " ;
-$def[1] .= "GPRINT:var2:AVERAGE:\"%7.2lf %Sb/s avg\" " ;
-$def[1] .= "GPRINT:var2:MAX:\"%7.2lf %Sb/s max\\n\" ";
+$def[1] .= "GPRINT:var2:LAST:\"%7.2lf %SB/s last\" " ;
+$def[1] .= "GPRINT:var2:AVERAGE:\"%7.2lf %SB/s avg\" " ;
+$def[1] .= "GPRINT:var2:MAX:\"%7.2lf %SB/s max\\n\" ";
 if($this->MACRO['TIMET'] != ""){
     $def[1] .= "VRULE:".$this->MACRO['TIMET']."#000000:\"Last Service Check \\n\" ";
 }
