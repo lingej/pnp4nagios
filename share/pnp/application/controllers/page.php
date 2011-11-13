8bb78f5ceeea83a244d7ccd32e989826733e72b6
Joerg Linge <pitchfork@pnp4nagios.org>
2011-11-13 13:02:19 +0100

    search_box added to basket and pages

--- share/pnp/application/controllers/page.php
+++ share/pnp/application/controllers/page.php
@@ -19,6 +19,7 @@ class Page_Controller extends System_Controller  {
         $this->template->page->graph_content->graph_width = ($this->config->conf['graph_width'] + 85);
         $this->template->page->graph_content->timerange_select = $this->add_view('timerange_select');
         $this->template->page->header         = $this->add_view('header');
+        $this->template->page->search_box     = $this->add_view('search_box');
         $this->template->page->logo_box       = $this->add_view('logo_box');
         $this->is_authorized=TRUE;
     }
@@ -38,19 +39,19 @@ class Page_Controller extends System_Controller  {
             $this->view = $this->config->conf['overview-range'];
         }
         $this->data->buildPageStruct($this->page,$this->view);
-        $this->template->page->header->title = Kohana::lang('common.page',$this->data->PAGE_DEF['page_name']);
+        $this->template->page->header->title      = Kohana::lang('common.page',$this->data->PAGE_DEF['page_name']);
         $this->url = "?page&page=$this->page";
-           // Timerange Box Vars
-           $this->template->page->timerange_box = $this->add_view('timerange_box');
-           $this->template->page->timerange_box->timeranges = $this->data->TIMERANGE;
+        // Timerange Box Vars
+        $this->template->page->timerange_box      = $this->add_view('timerange_box');
+        $this->template->page->timerange_box->timeranges = $this->data->TIMERANGE;
         // Pages Box
         $this->pages = $this->data->getPages();
-           $this->template->page->pages_box = $this->add_view('pages_box');
-           $this->template->page->pages_box->pages = $this->pages;
+        $this->template->page->pages_box          = $this->add_view('pages_box');
+        $this->template->page->pages_box->pages   = $this->pages;
         // Basket Box
-        $this->template->page->basket_box      = $this->add_view('basket_box');
+        $this->template->page->basket_box         = $this->add_view('basket_box');
         // Icon Box    
-        $this->template->page->icon_box      = $this->add_view('icon_box');
+        $this->template->page->icon_box           = $this->add_view('icon_box');
         $this->template->page->icon_box->position = "page";
 
     }
@@ -59,18 +60,18 @@ class Page_Controller extends System_Controller  {
         $basket = $this->session->get("basket");
         if(is_array($basket) && sizeof($basket) > 0){
             $this->data->buildBasketStruct($basket,$this->view);
-            $this->template->page->basket_box      = $this->add_view('basket_box');
-            $this->template->page->header->title = Kohana::lang('common.page-basket');
+            $this->template->page->basket_box         = $this->add_view('basket_box');
+            $this->template->page->header->title      = Kohana::lang('common.page-basket');
             $this->url = "basket?";
-               // Timerange Box Vars
-               $this->template->page->timerange_box = $this->add_view('timerange_box');
-               $this->template->page->timerange_box->timeranges = $this->data->TIMERANGE;
-                // Pages Box
+            // Timerange Box Vars
+            $this->template->page->timerange_box      = $this->add_view('timerange_box');
+            $this->template->page->timerange_box->timeranges = $this->data->TIMERANGE;
+            // Pages Box
             $this->pages = $this->data->getPages();
-               $this->template->page->pages_box = $this->add_view('pages_box');
-               $this->template->page->pages_box->pages = $this->pages;
+            $this->template->page->pages_box          = $this->add_view('pages_box');
+            $this->template->page->pages_box->pages   = $this->pages;
             // Icon Box    
-            $this->template->page->icon_box      = $this->add_view('icon_box');
+            $this->template->page->icon_box           = $this->add_view('icon_box');
             $this->template->page->icon_box->position = "basket";
         }else{
             url::redirect("start", 302);
