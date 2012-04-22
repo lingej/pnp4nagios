be1275563ce0e679472585087b2af40fdbb5cc58
Joerg Linge <pitchfork@pnp4nagios.org>
2012-04-22 14:23:57 +0200

    Don't rely on REQUEST_URI to run in CLI mode

--- share/pnp/application/views/kohana_error_page.php
+++ share/pnp/application/views/kohana_error_page.php
@@ -3,7 +3,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php if(isset ( $_SERVER['REQUEST_URI'])):?> 
 <meta http-equiv="refresh" content="60; url=<?php echo $_SERVER['REQUEST_URI'] ?>">
+<?php endif ?>
 <title><?php echo $error ?></title>
 <?php echo html::stylesheet('media/css/common.css') ?>
 <?php echo html::stylesheet('media/css/ui-'.Kohana::config('core.theme').'/jquery-ui.css') ?>
