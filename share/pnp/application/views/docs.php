0c1a639b9ae0bb609aa323a6487a434f9407c656
Joerg Linge <pitchfork@pnp4nagios.org>
2012-09-07 20:53:26 +0200

    fixed html tags

--- share/pnp/application/views/docs.php
+++ share/pnp/application/views/docs.php
@@ -2,30 +2,30 @@
 <table class="body">
 <tr valign="top"><td>
 <div class="left ui-widget">
- <div class="p2 ui-widget-header ui-corner-top">
- <?php echo Kohana::lang('common.docs-header',PNP_VERSION) ?>
- </div>
+<div class="p2 ui-widget-header ui-corner-top">
+<?php echo Kohana::lang('common.docs-header',PNP_VERSION) ?>
+</div>
 <div class="p4 ui-widget-content ui-corner-bottom" style="width: <?php echo $this->graph_width ?>px">
-	<?php if (!empty($this->content)) {
-     		echo $this->content;
-	} ?>
-    </div>
-    </td><td>
-	<div class="right">
+<?php if (!empty($this->content)) {
+echo $this->content;
+} ?>
+</div>
+</div>
+</td><td>
+<div class="right">
 
-	<?php if (!empty($docs_box)) {
-     		echo $docs_box;
-	} ?>
+<?php if (!empty($docs_box)) {
+echo $docs_box;
+} ?>
 
-	<?php if (!empty($logo_box)) {
-     		echo $logo_box;
-	} ?>
-	</div>
-    </td></tr>
-	<tr valign="top"><td colspan="2">
-    <div class="cb p4 ui-widget-content ui-corner-all">
-	<?php echo Kohana::lang('core.stats_footer') ?>
-    </div>
-    </div>
-	</td></tr></table>
+<?php if (!empty($logo_box)) {
+echo $logo_box;
+} ?>
+</div>
+</td></tr>
+<tr valign="top"><td colspan="2">
+<div class="cb p4 ui-widget-content ui-corner-all">
+<?php echo Kohana::lang('core.stats_footer') ?>
+</div>
+</td></tr></table>
 </div>
