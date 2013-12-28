ef75b216294a6a74ec1a6cea547254daf2b5babc
Ricardo Bartels <ricardo@bitchbrothers.com>
2013-12-28 22:14:22 +0100

    add "version=tiny" to got get i stripped down version of grap
    if you add version=tiny to the graph url it opens a minified
    version of graph view. this is usefull in popup windows.

---
+++ share/pnp/application/views/graph_tiny.php
@@ -0,0 +1,19 @@
+<div style="padding:4px">
+    <div style="display: table; margin: 0 auto;">
+	<table class="body">
+	    <tr valign="top">
+		<td>
+		    <div class="left ui-widget-content ui-corner-all" align="center">
+			<?php if (!empty($icon_box)) {
+     			    echo $icon_box;
+			} ?>
+
+			<?php if (!empty($graph_content)) {
+     			    echo $graph_content;
+			} ?>
+		    </div>
+		</td>
+	    </tr>
+	</table>
+    </div>
+</div>
