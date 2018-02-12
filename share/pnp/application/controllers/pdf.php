300c5d60beb0df2baaa0d4b424e852067a079e4f
wnieder <wnd@gmx.net>
2018-02-12 15:16:08 +0100

    remove "sizeof" (issue with PHP >= 7.2)

--- share/pnp/application/controllers/pdf.php
+++ share/pnp/application/controllers/pdf.php
@@ -202,7 +202,7 @@ class Pdf_Controller extends System_Controller  {
         }
         $this->data->getTimeRange($this->start,$this->end,$this->view);
         $basket = $this->session->get("basket");
-        if(is_array($basket) && sizeof($basket) > 0){
+        if(is_array($basket) && (!empty($basket))){
              $this->data->buildBasketStruct($basket,$this->view);
         }
         //echo Kohana::debug($this->data->STRUCT);
--- share/pnp/templates.dist/pnp-runtime.php
+++ share/pnp/templates.dist/pnp-runtime.php
@@ -34,7 +34,7 @@ $opt[2] = "--vertical-label \"Counter\" --title \"Number of updates\" ";
 #
 $ds_name[2] = "Errors and updates";
 $def[2] = '';
-for($i=2; $i <= sizeof($DS); $i++) {
+for($i=2; $i <= count($DS); $i++) {
 $def[2] .=  rrd::def("var$i", $RRDFILE[$i], $DS[$i] , "AVERAGE") ;
 $def[2] .= rrd::line1("var$i", rrd::color($i), rrd::cut(ucfirst($LABEL[$i]),12) );
 $def[2] .= rrd::gprint("var$i", array('LAST', 'MAX', 'AVERAGE'), "%4.0lf$UNIT[$i]");
