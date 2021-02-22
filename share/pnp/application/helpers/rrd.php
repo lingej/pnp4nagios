e2e1b025429a7e92212fc836246f2d9250a4c432
jimbobmcgee <jimbobmcgee@users.noreply.github.com>
2021-02-22 20:40:05 +0000

    Revert "rrd.php: added helper 'colbright' to adjust the brightness of predefined colors."
    This reverts commit 9302bc84366a3ee974dc829aafbcc34dea1569d3.

--- share/pnp/application/helpers/rrd.php
+++ share/pnp/application/helpers/rrd.php
@@ -444,35 +444,6 @@ class rrd_Core {
 		return $line;
 	}
 
-    # http://stackoverflow.com/questions/3512311/how-to-generate-lighter-darker-color-with-php
-    public static function colbright($hex, $steps) {
-        if($hex === FALSE){
-            throw new Kohana_exception("rrd::". __FUNCTION__ . "() First Parameter 'hex' is missing");
-        }
-        if($steps === FALSE){
-            throw new Kohana_exception("rrd::". __FUNCTION__ . "() Second Parameter 'steps' is missing");
-        }
-        // Steps should be between -255 and 255. Negative = darker, positive = lighter
-        $steps = max(-255, min(255, $steps));
-
-        // Normalize into a six character long hex string
-        $hex = str_replace('#', '', $hex);
-        if (strlen($hex) == 3) {
-            $hex = str_repeat(substr($hex,0,1), 2).str_repeat(substr($hex,1,1), 2).str_repeat(substr($hex,2,1), 2);
-        }
-
-        // Split into three parts: R, G and B
-        $color_parts = str_split($hex, 2);
-        $return = '#';
-
-        foreach ($color_parts as $color) {
-            $color   = hexdec($color); // Convert to decimal
-            $color   = max(0,min(255,$color + $steps)); // Adjust color
-            $return .= str_pad(dechex($color), 2, '0', STR_PAD_LEFT); // Make two char hex code
-        }
-        return $return;
-    }
-
     public static function debug($data=FALSE){
         if($data != FALSE){
 	    ob_start();
